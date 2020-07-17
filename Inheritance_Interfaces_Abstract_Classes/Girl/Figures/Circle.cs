c88c8d72cdc595881fe572f77a9cbe59e1207969
zalyz <54811025+zalyz@users.noreply.github.com>
2020-07-17 21:53:45 +0300

    Creating Figures

---
+++ Inheritance_Interfaces_Abstract_Classes/Girl/Figures/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girl.Figures
+{
+    public class Circle : Figure
+    {
+        public override double[] SidesLength { get; } = new double[1];
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(SidesLength[0], 2);
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * SidesLength[0];
+        }
+    }
+}
---
+++ Inheritance_Interfaces_Abstract_Classes/Girl/Figures/Figure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girl.Figures
+{
+    enum FigureColor
+    {
+        Transparent,
+        White,
+        Red,
+        Blue,
+        Yellow,
+        Green
+    }
+
+    enum FigureMaterial
+    {
+        Film = 1,
+        Paper = 2
+    }
+
+    public abstract class Figure
+    {
+        private bool _isPaintable;
+
+        public abstract double[] SidesLength { get; }
+
+        public FigureColor Color { get; private set; }
+
+        public virtual void PaintFigureIn(FigureColor color)
+        {
+            if (_isPaintable && (Color == FigureColor.White))
+            {
+                Color = color;
+            }
+            else
+                throw new Exception();
+        }
+
+        public abstract double GetArea();
+
+        public abstract double GetPerimeter();
+    }
+}
