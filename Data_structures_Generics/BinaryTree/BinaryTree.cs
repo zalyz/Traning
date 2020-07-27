04a6346ce39ba59b972670836facc243c9c99e5c
zalyz <54811025+zalyz@users.noreply.github.com>
2020-07-27 15:19:26 +0300

    Createing BinaryTree class.

---
+++ Data_structures_Generics/BinaryTree/BinaryTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T> where T : class
+    {
+        private Node _root;
+
+        public void Add(T record)
+        {
+            if (record == null)
+                throw new ArgumentNullException();
+
+            var nextNode = _root;
+            var comparableRecord = record as IComparable;
+            while (nextNode != null)
+            {
+                if (comparableRecord.CompareTo(nextNode.Value) <= 0)
+                {
+                    nextNode = nextNode.Left;
+                }
+                else
+                {
+                    nextNode = nextNode.Right;
+                }
+            }
+
+            nextNode = new Node(record) { Left = null, Right = null };
+        }
+
+        public void LeftTurn()
+        {
+            var oldRoot = _root;
+            var newRoot = _root.Right;
+            oldRoot.Right = newRoot.Left;
+            newRoot.Left = oldRoot;
+            _root = newRoot;
+        }
+
+        public void RightTurn()
+        {
+            var oldRoot = _root;
+            var newRoot = _root.Left;
+            oldRoot.Left = newRoot.Right;
+            newRoot.Right = oldRoot;
+            _root = newRoot;
+        }
+
+        private class Node
+        {
+            public T Value;
+
+            public Node Left;
+
+            public Node Right;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
---
+++ Data_structures_Generics/BinaryTree/TestResult.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class TestResult : IComparable
+    {
+        public string StudentName { get; private set; }
+        
+        public string TestName { get; private set; }
+        
+        public DateTime Date { get; private set; }
+
+        public int TestScore { get; private set; }
+
+        public TestResult(string studentName, string testName, DateTime date, int testScore)
+        {
+            StudentName = studentName;
+            TestName = testName;
+            Date = date;
+            TestName = testName;
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            return CompareTo(obj as TestResult);
+        }
+
+        int CompareTo(TestResult other)
+        {
+            if (other == null)
+                throw new ArgumentNullException();
+
+            if (TestScore == other.TestScore)
+            {
+                return 0;
+            }
+            else
+            {
+                if (TestScore < other.TestScore)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
